027314696bb37ff57d3d17235eadf422babf595c
Liviarodrigues1 <liviarodriguesre@gmail.com>
2025-05-04 19:33:12 -0300

    Fix (test) : alteração no teste

--- app/test_main.py
+++ app/test_main.py
@@ -1,20 +1,21 @@
-# Função de teste comentada por enquanto, pois não será usada agora
+# import sys
+# import os
+
+# Garante que o diretório raiz do projeto esteja no sys.path
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+# from fastapi.testclient import TestClient
+# from app.main import app
 
 # def test_hello_world():
-#     # Importa o TestClient da FastAPI para simular requisições HTTP nos testes
-#     from fastapi.testclient import TestClient
-#     
-#     # Importa o objeto `app` da aplicação principal localizada em app/main.py
-#     from app.main import app
-#     
-#     # Cria um cliente de teste com a aplicação
-#     client = TestClient(app)
-#     
-#     # Envia uma requisição GET para o endpoint raiz "/"
-#     response = client.get("/")
-#     
-#     # Verifica se o status da resposta é 200 (OK)
-#     assert response.status_code == 200
-#     
-#     # Verifica se o conteúdo da resposta corresponde ao esperado
-#     assert response.json() == {"message": "Hello, World!"}
+    # Cria um cliente de teste com a aplicação
+    # client = TestClient(app)
+    
+    # Envia uma requisição GET para o endpoint raiz "/"
+    # response = client.get("/")
+    
+    # Verifica se o status da resposta é 200 (OK)
+    # assert response.status_code == 200
+    
+    # Verifica se o conteúdo da resposta corresponde ao esperado
+    # assert response.json() == {"message": "Hello, World!"}
