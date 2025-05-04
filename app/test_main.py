fdf44715759e887b5914c3ce517ad47a0d24ba3c
Liviarodrigues1 <liviarodriguesre@gmail.com>
2025-05-04 15:38:01 -0300

    Feat (infra): criação inicial da infraestrutura com Docker, FastAPI, PostgreSQL, pgAdmin, testes e arquivos de configuração

---
+++ app/test_main.py
@@ -0,0 +1,7 @@
+# def test_hello_world():
+#     from fastapi.testclient import TestClient
+#     from app.main import app
+#     client = TestClient(app)
+#     response = client.get("/")
+#     assert response.status_code == 200
+#     assert response.json() == {"message": "Hello, World!"}
